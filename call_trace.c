242345757eae779a6aeec2ed6e6a3b8a6caf2e40
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2015-10-29 12:02:08 +0100

    call_trace.c: retrieve full function name
    The previous version would only get the first 255 bytes. The new one will
    keep trying unw_get_proc_name with bigger buffers until it succeeds.
    Includes documentation.

--- call_trace.c
+++ call_trace.c
@@ -7,6 +7,31 @@
 #include <demangle.h>
 
 /*
+ * Retrieves the full name of the procedure name from cursor using
+ * unw_get_proc_name multiple times if necessary. Returns NULL if procedure
+ * name can't be retrieved. The returned pointer needs to be freed.
+ */
+static char *get_full_proc_name(unw_cursor_t *cursor, unw_word_t *offp)
+{
+	int len = 256;
+	char *name = malloc(sizeof(*name) * len);
+	int ret = unw_get_proc_name(cursor, name, len, offp);
+
+	if (ret == UNW_ENOMEM) {
+		do {
+			len *= 1.4;
+			name = realloc(name, len);
+			ret = unw_get_proc_name(cursor, name, len, offp);
+		} while (ret == UNW_ENOMEM);
+	} else if (ret == UNW_ENOINFO || ret == UNW_EUNSPEC) {
+		perror("unw_get_proc_name");
+		free(name);
+		name = NULL;
+	}
+	return name;
+}
+
+/*
  * Prints a backtrace using libunwind. The printed format is:
  *
  *	#<number> <demangled function name>+<hexadecimal pointer offset>
@@ -23,17 +48,18 @@ void show_backtrace(void)
 	unw_init_local(&cursor, &uc);
 
 	while (unw_step(&cursor) > 0) {
-		char fname[256];
-
 		unw_get_reg(&cursor, UNW_REG_IP, &ip);
 		unw_get_reg(&cursor, UNW_REG_SP, &sp);
-		unw_get_proc_name(&cursor, fname, 256, &offp);
-		char *realname = cplus_demangle(fname, DMGL_PARAMS|DMGL_AUTO);
-		char *name = realname ? realname : fname;
-		printf("#%d %s+%#lx\n", ctr, name, offp);
-		free(realname);
+
+		char *fname = get_full_proc_name(&cursor, &offp);
+		char *realname = cplus_demangle(fname, DMGL_PARAMS | DMGL_AUTO);
+		const char *name = realname ? realname : fname;
+
+		printf("#%d %s+%#lx\n", ctr, name, (long)offp);
 
 		++ctr;
+		free(realname);
+		free(fname);
 	}
 }
 
